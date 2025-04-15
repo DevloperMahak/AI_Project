4571fccb75b70af5b98cca08e57aa3cdab4ea0b7
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-04-16 00:34:48 +0530

    authentication path added

--- askmytutor_backend/routes/userRoutes.js
+++ askmytutor_backend/routes/userRoutes.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const multer = require('multer');
-const { extractTextFromImage,getAnswer } = require('../controllers/userController');
+const fs = require('fs');
+const path = require('path');
+const { extractTextFromImage,getAnswer,signupUser,loginUserController } = require('../controllers/userController');
 
 const router = express.Router();
 
+
+// ✅ Resolve absolute path to root-level uploads folder
+const uploadDir = path.join(__dirname, '..', 'uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 // Multer setup for file uploads
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'uploads/'),
+  destination: (req, file, cb) => {
+    cb(null, uploadDir); // points outside `src`
+  },
   filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname),
 });
 
@@ -18,5 +29,10 @@ router.post('/upload-image', upload.single('image'), extractTextFromImage);
 // POST route for getting answer from GROQ
 router.post('/ask', getAnswer);
 
+// POST route for getting User Details
+router.post('/signup', signupUser);
+
+// Route to handle user login
+router.post('/login', loginUserController);
 
 module.exports = router;
