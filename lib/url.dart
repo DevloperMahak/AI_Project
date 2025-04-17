3f6fd00078288eeda56a3f3f1098d4b4b51d8dc8
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-04-17 11:25:03 +0530

    path added

--- lib/url.dart
+++ lib/url.dart
@@ -1,2 +1,5 @@
 final url = 'http://192.168.58.59:5001/api/user/';
 final doubt = url + "ask";
+final doubt_img = url + "upload-image";
+final sign_up = url + "signup";
+final login = url + "login";
