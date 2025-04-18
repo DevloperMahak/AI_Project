663b3de15a410d2e1728da0fa358e69cc714c861
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-04-18 21:52:45 +0530

    backend url added

--- lib/url.dart
+++ lib/url.dart
@@ -1,4 +1,4 @@
-final url = 'http://192.168.58.59:5001/api/user/';
+final url = 'https://askmytutor-backend.onrender.com/api/user/';
 final doubt = url + "ask";
 final doubt_img = url + "upload-image";
 final sign_up = url + "signup";
