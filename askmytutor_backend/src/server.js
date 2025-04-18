f0515377c719a2e55e97ccb1b82eff8995c1dc12
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-04-18 21:06:25 +0530

    port changed

--- askmytutor_backend/src/server.js
+++ askmytutor_backend/src/server.js
@@ -34,7 +34,7 @@ io.on('connection', (socket) => {
 });
 
 connectDB();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());// Parses incoming JSON bodies
 app.use(cors());// Enables cross-origin requests
