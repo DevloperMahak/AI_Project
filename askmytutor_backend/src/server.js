cd0b1526d4ca233d5b73b60a88b3b63adee779d5
DeveloperMahak <mahakguptaji2005@gmail.com>
2026-03-02 20:08:18 +0530

    render path added instead of vercel

--- askmytutor_backend/src/server.js
+++ askmytutor_backend/src/server.js
@@ -44,7 +44,7 @@ app.use(express.json()); // Parses incoming JSON bodies
 // Allow requests from your frontend URL
 app.use(
   cors({
-    origin: "https://askmytutor-l6m0nu1np-devlopermahaks-projects.vercel.app", // replace with your frontend URL
+    origin: "https://askmytutor.onrender.com", // replace with your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
